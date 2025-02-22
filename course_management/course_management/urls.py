89c57064d0299ff1455cdbabba7757b0b6453b8c
Daca <dacaldes@gmail.com>
2025-02-22 23:09:50 +0100

    swagger

--- course_management/course_management/urls.py
+++ course_management/course_management/urls.py
@@ -19,11 +19,30 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Course Manager API",
+        default_version='v1',
+        description="Documentation for the Course Manager API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@coursemanager.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('courses.urls')), 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtainer'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 from django.conf import settings
